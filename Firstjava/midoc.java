af04b0c3d8247bd46a3302db3c4c2fc55fc7244c
lLuispD <luispdedios@gmail.com>
2024-04-18 02:50:08 -0500

    cambios minimos

--- Firstjava/midoc.java
+++ Firstjava/midoc.java
@@ -1,3 +1,7 @@
+/**
+ *  @author luis pDedios <luispdedios@gmail.com> 
+ */
+
 import java.util.Scanner;
 
 public class midoc {
@@ -5,13 +9,14 @@ public class midoc {
         Scanner in = new Scanner(System.in);
         String nombre = "";
         int num1 = 5, num2 = 0, num3 = 0;
-        System.out.println("Introdiuce tu nombre");
+        System.out.println("Digite su nombre");
         nombre = in.nextLine();
-        System.out.println("muy bien, " + nombre + ", ahora ingresa un nomero");
+        System.out.println("Hola " + nombre + ", por favor ingresa un numero");
         num1 = in.nextInt();
-        System.out.println("Ahora agrega otro numero");
+        System.out.println("Ahora ingresa otro numero");
         num2 = in.nextInt();
         num3 = num1 + num2;
+
         System.out.println("La suma de " + num1 + " + " + num2 + " es igual a: " + num3);
     }
     
--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 
 Este repositorio estara dedicado a los primeros pasos de programacion en Java. 
 y tambien otras cosas relacionadas a este lenguaje de programacion
+
+Se desarrollara en el entorno de Visual Studio code:
+    Version: 1.88.1 (user setup)
+    Commit: e170252f762678dec6ca2cc69aba1570769a5d39
+    Date: 2024-04-10T17:41:02.734Z
+    Electron: 28.2.8
+    ElectronBuildId: 27744544
+    Chromium: 120.0.6099.291
+    Node.js: 18.18.2
+    V8: 12.0.267.19-electron.0
+    OS: Windows_NT x64 10.0.22631
+
+Version de JDK Java 22
